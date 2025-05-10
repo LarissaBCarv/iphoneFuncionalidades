de141362b210cecb7d79a7cc34063e5d886a3328
LarissaBCarv <l.carvaborges@gmail.com>
2025-05-10 11:44:15 -0300

    Create Iphone.java

---
+++ Iphone.java
@@ -0,0 +1,34 @@
+classDiagram
+    class ReprodutorMusical {
+        +tocar()
+        +pausar()
+        +selecionarMusica(musica: String)
+    }
+
+    class AparelhoTelefonico {
+        +ligar(numero: String)
+        +atender()
+        +iniciarCorreioVoz()
+    }
+
+    class NavegadorInternet {
+        +exibirPagina(url: String)
+        +adicionarNovaAba()
+        +atualizarPagina()
+    }
+
+    class Iphone {
+        +tocar()
+        +pausar()
+        +selecionarMusica(musica: String)
+        +ligar(numero: String)
+        +atender()
+        +iniciarCorreioVoz()
+        +exibirPagina(url: String)
+        +adicionarNovaAba()
+        +atualizarPagina()
+    }
+
+    ReprodutorMusical <|-- Iphone
+    AparelhoTelefonico <|-- Iphone
+    NavegadorInternet <|-- Iphone
